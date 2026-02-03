af438492ec04123ce9658063fec2bb6b4f582669
JhonatanMrls <jhonatanmorales99@gmail.com>
2026-02-03 16:39:16 -0500

    feat: Add JSON serializer option to convert enums to strings.

--- RestaurantBackend.API/Program.cs
+++ RestaurantBackend.API/Program.cs
@@ -1,4 +1,5 @@
 using System.Text;
+using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
@@ -72,7 +73,11 @@ builder.Services.AddCors(options =>
     });
 });
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
 builder.Services.AddEndpointsApiExplorer();
 
 // 5. Swagger Configuration with JWT
